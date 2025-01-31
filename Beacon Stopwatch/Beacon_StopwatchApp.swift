b703f98cfb0b4765319124072a3e8e56fc39cce6
Aidan Lloyd-Tucker <aidanlloydtucker@gmail.com>
2025-01-31 09:11:10 -0800

    Initial Commit

---
+++ Beacon Stopwatch/Beacon_StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Beacon_StopwatchApp.swift
+//  Beacon Stopwatch
+//
+//  Created by Aidan Lloyd-Tucker on 1/31/25.
+//
+
+import SwiftUI
+
+@main
+struct Beacon_StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
